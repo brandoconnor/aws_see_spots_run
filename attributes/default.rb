d6585b7dfa8f5f4abe086c2e7fdf99e736b736a9
brandoconnor <brandoconnor@gmail.com>
2014-12-30 18:19:36 -0800

    Added all args, sorted attributes, and reformatted a list arg to actually be a string

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 
 default['AWS_see_spots_run']['exec_path'] = '/usr/local/bin/'
 default['AWS_see_spots_run']['min_healthy_AZs'] = 3
-default['AWS_see_spots_run']['excluded_regions'] = [ 'cn-north-1', 'us-gov-west-1','us-east-1' ] #XXX include east-1 when everything is solid
+default['AWS_see_spots_run']['excluded_regions'] = 'cn-north-1 us-gov-west-1 us-east-1' #XXX include east-1 when everything is solid
 
 default['AWS_see_spots_run']['spot_request_killer']['interval'] = 5
 default['AWS_see_spots_run']['spot_request_killer']['minutes_before_stale'] = 10
@@ -14,6 +14,5 @@ default['AWS_see_spots_run']['price_monitor']['interval'] = 10
 
 default['AWS_see_spots_run']['ASG_tagger']['interval'] = 30
 
-default['AWS_see_spots_run']['health_enforcer']['interval'] = 10
+default['AWS_see_spots_run']['health_enforcer']['interval'] = 15
 default['AWS_see_spots_run']['health_enforcer']['demand_expiration'] = 50
-
