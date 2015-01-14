c65669d639b5a17e609459a1eb59ffa7672e3cdf
brandoconnor <brandoconnor@gmail.com>
2015-01-13 17:58:46 -0800

    removing an internal dependency - cronwrappy

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,8 @@ name             'AWS_see_spots_run'
 maintainer       "DreamBox Learning, Inc."
 maintainer_email 'brandoconnor@gmail.com'
 license          'Apache 2.0'
-description      'Installs/Configures AWS_see_spots_run'
+description      'Installs/Configures AWS_see_spots_run a spot instance management platform.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'python'
-depends          'cronwrappy'
