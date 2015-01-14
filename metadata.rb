a78339076ed16f7884b31b95791fa4ad81b38637
brandoconnor <brandoconnor@gmail.com>
2015-01-13 18:06:09 -0800

    added metadata details for completeness

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'brandoconnor@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures AWS_see_spots_run, a spot instance management platform.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-issues_url       'https://github.com/dreamboxlearning/AWS_see_spots_run/issues'
-source_url       'https://github.com/dreamboxlearning/AWS_see_spots_run'
+#source_url       'https://github.com/dreamboxlearning/AWS_see_spots_run'
+#issues_url       'https://github.com/dreamboxlearning/AWS_see_spots_run/issues'
 version          '0.1.2'
 depends          'python'
 
